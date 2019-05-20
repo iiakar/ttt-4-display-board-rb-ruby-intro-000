7b722a38b72329cf3d889bc461e23b54267272db
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-20 16:04:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,24 +9,31 @@ def display_board(board)
  
  if board = [" "," "," "," "," "," "," "," "," "]
  
- puts rows[0] = ("   |   |   ")
-      rows[1] = ("-----------")
-      rows[2] = ("   |   |   ")
-      rows[3] = ("-----------")
-      rows[4] = ("   |   |   ")
-
-else if board = [" "," "," "," "," X "," "," "," "," "]
-  puts board 
-    rows[0] = ("   |   |   ")
-    rows[1] = ("-----------")
-    rows[2] = ("   | X |   ")
-    rows[3] = ("-----------")
-    rows[4] = ("   |   |   ")
+  
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
 
-end 
-end 
-end 
+
+
+
+ #puts rows[0] = ("   |   |   ")
+  #    rows[1] = ("-----------")
+   #   rows[2] = ("   |   |   ")
+    #  rows[3] = ("-----------")
+     # rows[4] = ("   |   |   ")
+
+#else if board = [" "," "," "," "," X "," "," "," "," "]
+ # puts board 
+  #  rows[0] = ("   |   |   ")
+   # rows[1] = ("-----------")
+    #rows[2] = ("   | X |   ")
+    #rows[3] = ("-----------")
+    #rows[4] = ("   |   |   ")
  
  
  # puts board 
