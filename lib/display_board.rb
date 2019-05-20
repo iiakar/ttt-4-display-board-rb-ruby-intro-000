15c3a00efb6fa72bb57169f6c9cd436d14e023a6
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-20 16:10:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -16,8 +16,12 @@ def display_board(board)
  
  elsif 
  puts board = [" "," "," "," "," X "," "," "," "," "]
-
-  
+ 
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end 
 end
 
