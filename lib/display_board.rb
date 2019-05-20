5ca4d1e512732bc54234249c383c1111ff1b95c5
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-20 16:06:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@
 
 def display_board(board)
  
- board = [" "," "," "," "," "," "," "," "," "]
+ if board = [" "," "," "," "," "," "," "," "," "]
  
   
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -13,7 +13,8 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+ 
+ elsif 
 
 
 
